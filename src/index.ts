034cab2a17b2219617c4e08259d38053ee87c364
Haerul <haerulrn20@gmail.com>
2026-03-23 11:51:10 +0700

    feat: tambah function greet - closes #1

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,8 @@ halo = 123;
 halo = "Halo Dunia";
 console.log(nama)
 console.log({nama})
+
+function greet(name: string): string {
+    console.log(`Hello, ${name}!`);
+    return `Hello, ${name}!`;
+}
