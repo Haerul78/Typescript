f966a983e9cafdcf0d9edd9d582ed687e95509e3
Haerul <haerulrn20@gmail.com>
2026-03-23 11:46:06 +0700

    Update

---
+++ bundle/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let nama = "Haerul";
+console.log(nama);
+console.log({ nama });
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+let nama: string = "Haerul";
+let halo;
+
+halo = 123;
+halo = "Halo Dunia";
+console.log(nama)
+console.log({nama})
